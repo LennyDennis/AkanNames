8619215fc1c950809290d19596f8b46363a3dde7
LennyDennis <lennydennis@gmail.com>
2019-12-01 22:46:08 +0300

    allows js to get input from the form and create js variables.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//getting date input from the form
+var mydate = document.getElementById("mydate");
+var gender = document.akanform.gender;
+
+var bday = mydate.value;
+var date = new Date(document.getElementById("mydate").value);
+var dateday = date.getDay();
+var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var day = weekdays[dateday];
+var mName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var fName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var valid = false;
+
+
